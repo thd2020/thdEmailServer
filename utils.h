d4869f6347b6d3fdb2beee4687fedfb8d1854afe
thd2020 <74453079+thd2020@users.noreply.github.com>
2023-11-21 10:56:04 +0800

    Update and rename register.c to utils.h

---
+++ utils.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#ifndef UTILS_H
+#define UTILS_H
+
+// 基础路径
+#define BASE_PATH "/mailserver/users"
+// 用户名长度
+#define USERNAME_LENGTH 30
+
+// 注册和创建用户邮箱空间
+int register_user(char *username) 
+
+#endif /* REGISTER_H */
