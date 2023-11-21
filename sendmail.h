ab85eeea325fec665444df30028cfdb320e19618
thd2020 <74453079+thd2020@users.noreply.github.com>
2023-11-21 11:32:12 +0800

    Update sendmail.h

--- sendmail.h
+++ sendmail.h
@@ -1,5 +1,12 @@
 #ifndef SENDMAIL_H
 #define SENDMAIL_H
 
-#define SMTP_SERVER "smtp.gmail.com"
+#define SMTP_SERVER "smtp.thd2020.site"
 #define SMTP_PORT 587
+typedef struct{
+  char *from = "\"User Name\" <username@gmail.com>";
+  char *to = "\"Recipient Name\" <recipient@gmail.com>";
+  char *subject = "Hello, world!";
+  char *body = "This is a test email sent from a simple SMTP client.";
+} MailContent
+#endif
