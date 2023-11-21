9fd86c28d199b47eef6108a23601b33a00f01c8c
thd2020 <74453079+thd2020@users.noreply.github.com>
2023-11-21 11:35:15 +0800

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,12 +1,19 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-// 基础路径
+#include <stdlib.h>  
+#include <string.h>  
+
+/**基础路径**/
 #define BASE_PATH "/mailserver/users"
-// 用户名长度
+/**用户名长度**/
 #define USERNAME_LENGTH 30
 
-// 注册和创建用户邮箱空间
+/**BASE64编码函数**/
+unsigned char *base64_encode(unsigned char *str);
+/**BASE64解码函数**/
+unsigned char *base64_decode(unsigned char *code);
+/**用户注册**/
 int register_user(char *username) 
 
 #endif /* REGISTER_H */
